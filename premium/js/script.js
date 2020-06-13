85309a92cd6d34cbfc9294cbf788f421e0361283
lavaresones <tyrbocompany@gmail.com>
2020-06-13 12:16:54 +0300

    add index.html

---
+++ premium/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.main-slider-for-plugin').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 400 
+    });
+  });
